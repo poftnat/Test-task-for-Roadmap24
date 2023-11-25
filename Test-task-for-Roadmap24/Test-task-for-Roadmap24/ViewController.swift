d50bc9ce3d4a1d3a334c3d5b70cbf1bc111a826d
poftnat <149266332+poftnat@users.noreply.github.com>
2023-11-25 13:02:45 +0300

    init

---
+++ Test-task-for-Roadmap24/Test-task-for-Roadmap24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-task-for-Roadmap24
+//
+//  Created by Наталья Владимировна Пофтальная on 25.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
